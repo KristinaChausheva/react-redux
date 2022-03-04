3a568015b093d2f7e70244b9e8a1c3e9abf8f2c3
Kristina Chausheva <k.chausheva@gmail.com>
2022-03-04 03:25:21 +0200

    adding redux toolkit and refreactoring

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import "./App.css"
 import { useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { increment, decrement, incrementByAmount } from "./redux/counter"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { count } = useSelector((state) => state.counter)
+  const dispatch = useDispatch()
   return (
     <div className="App">
       <h1>The counter is : {count}</h1>
-      <button onClick={() => setCount(count + 1)}>increment</button>
-      <button onClick={() => setCount(count - 1)}>decrement</button>
+      <button onClick={() => dispatch(increment())}>increment</button>
+      <button onClick={() => dispatch(decrement())}>decrement</button>
+      <button onClick={() => dispatch(incrementByAmount(10))}>
+        Increment by 10
+      </button>
     </div>
   )
 }
